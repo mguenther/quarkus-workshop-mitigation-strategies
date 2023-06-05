8fc129f7a87b3635a72f6342ef355ac3934b5ff2
Markus Günther <markus.guenther@gmail.com>
2023-06-05 13:37:10 +0200

    Adds solution to task 3

--- src/main/java/workshop/quarkus/reactive/RetryingConsumer.java
+++ src/main/java/workshop/quarkus/reactive/RetryingConsumer.java
@@ -1,30 +1,33 @@
 package workshop.quarkus.reactive;
 
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.unchecked.Unchecked;
 import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecord;
 import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecord;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.kafka.common.header.Headers;
-import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @ApplicationScoped
 public class RetryingConsumer {
 
     @Incoming("retries")
     @Outgoing("retries-out")
-    @Retry(delay = 100, maxRetries = 5)
-    public OutgoingKafkaRecord<Object, String> process(IncomingKafkaRecord<Object, String> record) {
-
-        final var traceId = header(record.getHeaders(), "trace");
-
-        if (Math.random() <= 0.1) {
-            throw new IllegalArgumentException("Something terrible just happened");
-        }
-
-        return OutgoingKafkaRecord.from(record).withHeader("trace", traceId);
+    public Uni<OutgoingKafkaRecord<Object, String>> streamProcessor(IncomingKafkaRecord<Object, String> record) {
+        return Uni.createFrom().item(record)
+                .onItem().transform(Unchecked.function(m -> {
+                    if (Math.random() <= 0.1) {
+                        throw new IllegalArgumentException("Something terrible just happened");
+                    }
+                    final var traceId = header(m.getHeaders(), "trace");
+                    return OutgoingKafkaRecord.from(record).withHeader("trace", traceId);
+                }))
+                .onItem().invoke(record::ack)
+                .onFailure().retry().withBackOff(Duration.ofMillis(100)).atMost(5);
     }
 
     private String header(final Headers headers, final String key) {
